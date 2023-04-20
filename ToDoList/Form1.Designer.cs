4bddd922a959b24f2d0479c2fc8d7bc095bc0f1d
220170 <220170@G101-12126>
2023-04-20 12:21:54 +0100

    Add project files.

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddTaskBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddTaskButton = new System.Windows.Forms.Button();
+            this.TaskList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AddTaskBox
+            // 
+            this.AddTaskBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.AddTaskBox.Location = new System.Drawing.Point(285, 39);
+            this.AddTaskBox.Name = "AddTaskBox";
+            this.AddTaskBox.Size = new System.Drawing.Size(241, 21);
+            this.AddTaskBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(206, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Task to add:";
+            // 
+            // AddTaskButton
+            // 
+            this.AddTaskButton.Location = new System.Drawing.Point(542, 39);
+            this.AddTaskButton.Name = "AddTaskButton";
+            this.AddTaskButton.Size = new System.Drawing.Size(75, 23);
+            this.AddTaskButton.TabIndex = 5;
+            this.AddTaskButton.Text = "Add Task";
+            this.AddTaskButton.UseVisualStyleBackColor = true;
+            this.AddTaskButton.Click += new System.EventHandler(this.AddTaskButton_Click);
+            // 
+            // TaskList
+            // 
+            this.TaskList.FormattingEnabled = true;
+            this.TaskList.Location = new System.Drawing.Point(209, 96);
+            this.TaskList.Name = "TaskList";
+            this.TaskList.ScrollAlwaysVisible = true;
+            this.TaskList.Size = new System.Drawing.Size(408, 290);
+            this.TaskList.TabIndex = 6;
+            this.TaskList.DoubleClick += new System.EventHandler(this.RemoveItem);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TaskList);
+            this.Controls.Add(this.AddTaskButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AddTaskBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox AddTaskBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddTaskButton;
+        private System.Windows.Forms.ListBox TaskList;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        List<string> ToDo = new List<string>();
+        string path = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + "/Resources/ToDoText.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AddTaskBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(CheckEnterKeyPress);
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+
+                while ((s = sr.ReadLine()) != null)
+                {
+                    ToDo.Add(s);
+                }
+            }
+            ReloadChecklist();
+        }
+
+        private void ReloadChecklist()
+        {
+            TaskList.Items.Clear();
+            foreach (string s in ToDo)
+            {
+                TaskList.Items.Add(s);
+            }
+        }
+
+        private void CheckEnterKeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+
+            {
+                if (AddTaskBox.Text != "")
+                {
+                    ToDo.Add(AddTaskBox.Text);
+                    ReloadChecklist();
+                }
+            }
+        }
+
+        private void AddTaskButton_Click(object sender, EventArgs e)
+        {
+            if (AddTaskBox.Text != "") {
+                ToDo.Add(AddTaskBox.Text);
+                ReloadChecklist();
+            }
+        }
+
+        private void ItemChecked(object sender, ItemCheckEventArgs e)
+        {
+
+        }
+
+        private void RemoveItem(object sender, EventArgs e)
+        {
+            if (ToDo.Count != 0) {
+                ToDo.RemoveAt(TaskList.SelectedIndex);
+                ReloadChecklist();
+            }
+        }
+
+        private void OnClosing(object sender, FormClosingEventArgs e)
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                foreach (string s in ToDo)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+        }
+
+    }
+}
